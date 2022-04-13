d057650a870bf68ebfb13c9f4ee0fc50012d7c6e
AlikoGrace <gracealiko09@gmail.com>
2022-04-12 20:11:35 -0700

    put char

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,20 @@
 #include "main.h"
+
 /**
- * main - Entry for main function
- * Description: Program to print _putchar, followed by a line
- * Return: 0
+ * print_alphabet - print alphabet lowercase
+ *
+ * Return: Always 0.
  */
-int main(void)
+
+void print_alphabet(void)
 {
-char c[9] = "_putchar";
-int i = 0;
-while (i < 8)
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
-_putchar(c[i]);
-i++;
+_putchar(alphabet);
 }
+
 _putchar('\n');
-return (0);
+
 }
